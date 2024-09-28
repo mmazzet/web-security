8ae0fd189769b64d7514b4b2c227b1adf3b452f6
Marianna <20104120@mail.wit.ie>
2024-09-28 23:19:19 +0100

    implemented CSP

--- examples/csp-playground/index.js
+++ examples/csp-playground/index.js
@@ -1,10 +1,38 @@
 import { createServer, startServer } from '#shared';
+import helmet from "helmet";
 
 import { db } from './database.js';
 
 const app = createServer({
   log: true,
 });
+
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: true,
+  directives: {
+    defaultSrc: ["'none'"],
+    scriptSrc: ["'none'"],
+    styleSrc: ["'self'",'https://fonts.googleapis.com', 'https://cdn.jsdelivr.net',],
+    imgSrc: ["'self'", 'https://static.frontendmasters.com', 'https://fav.farm',],
+    fontSrc: ["'self'", 'https://fonts.googleapis.com', 'https://fonts.gstatic.com',],
+    connectSrc: ["'self'"],
+    objectSrc: ["'none'"],
+    upgradeInsecureRequests: [],
+    reportTo: 'csp-violation-report',
+    /* styleSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+    imgSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+    connectSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+    fontSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+    objectSrc: ["'self'"],
+    mediaSrc: ["'self'"],
+    frameSrc: ["'self'"],
+    workerSrc: ["'self'"], */
+
+  },
+  reportOnly: false,
+}));
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Content Security Policy' });
